120ad2b9b6627b381a0221347dc9e8625447b07a
IEVN1003-22002251 <78992@alumnos.utleon.edu.mx>
2025-09-05 18:48:06 -0600

    Segundo commit 05-09-2025

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import './temasTS/01-tipos-bas.ts'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+console.log("Hola mundo")
+
+var num1=23;
+let num2=32;
+const num3=67;
+
+let nombre:string 
+let num:number
+let activo:boolean
+
+let sum = num1+num2+num3;
+console.log("La suma es:" + sum);
+
+let matricula:number|string
+
+matricula=123456789
+matricula="SYH77665"
+console.log(matricula)
